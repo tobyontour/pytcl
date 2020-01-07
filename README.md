284ade4db557380b4a58b86ace6c0f2b1966b9f2
Toby Bettridge <toby.bettridge@stubside.com>
2020-01-07 23:39:03 +0000

    Inital commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pytcl
 Minimal Tcl DSL interpreter in Python
+
+## Notes
+https://python-packaging.readthedocs.io/en/latest/minimal.html
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pytcl',
+      version='0.1',
+      description='Minimal Tcl DSL interpreter in Python',
+      url='https://github.com/tobyontour/pytcl',
+      author='Toby Bettridge',
+      author_email='toby.bettridge@stubside.com',
+      license='MIT',
+      packages=['pytcl'],
+      zip_safe=False)
