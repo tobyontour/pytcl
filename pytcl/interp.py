655ef4b48a03aa54a72c1aeb443968075747deae
Toby Bettridge <toby@heavypenguin.com>
2020-06-16 20:03:16 +0100

    Set command.

--- pytcl/interp.py
+++ pytcl/interp.py
@@ -1,4 +1,8 @@
 
+
+class IncorrectNumberOfArgumentsError(Exception):
+    pass
+
 class Interpreter():
     """Main interpreter"""
 
@@ -13,3 +17,19 @@ class Interpreter():
             return self.variables[variable_name]
         except KeyError:
             return None
+
+    def eval(self, script):
+        tokens = script.split(' ')
+        if len(tokens) > 0:
+            self.call(tokens.pop(0), tokens)
+
+    def call(self, command, args):
+        if command == 'set':
+            if len(args) == 2:
+                if args[1][0].isdigit():
+                    self.variables[args[0]] = int(args[1])
+                else:
+                    self.variables[args[0]] = args[1]
+            else:
+                raise IncorrectNumberOfArgumentsError(f"set requires 2 arguments, {len(args)} given.")
+
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pytcl.interp import Interpreter
+from pytcl.interp import Interpreter, IncorrectNumberOfArgumentsError
 
 
 class TestInterp(unittest.TestCase):
@@ -17,11 +17,34 @@ class TestInterp(unittest.TestCase):
 
     def test_get_undefined_variable(self):
         """
-        Test that we can set and get a variable.
+        Test that get None for an undefined variable.
         """
         interp = Interpreter()
 
         self.assertEqual(interp.get('x'), None)
+
+    def test_simple_set_script(self):
+        interp = Interpreter()
+
+        interp.eval('set x 5')
+
+        self.assertEqual(interp.get('x'), 5)
+
+    def test_simple_set_script_incorrect_args(self):
+        interp = Interpreter()
+
+        with self.assertRaises(IncorrectNumberOfArgumentsError) as context:
+            interp.eval('set x')
+
+        self.assertTrue("set requires 2 arguments, 1 given." in str(context.exception))
+
+    def test_simple_set_script_incorrect_args_excess(self):
+        interp = Interpreter()
+
+        with self.assertRaises(IncorrectNumberOfArgumentsError) as context:
+            interp.eval('set x 5 4')
+
+        self.assertTrue("set requires 2 arguments, 3 given." in str(context.exception))
 
 if __name__ == '__main__':
     unittest.main()
