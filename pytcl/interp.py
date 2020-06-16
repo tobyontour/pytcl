139e053cb9a6b18ec5fc4b2c9ac007b7b1ed3d8b
Toby Bettridge <toby@heavypenguin.com>
2020-06-16 20:56:48 +0100

    Tokeniser

--- pytcl/interp.py
+++ pytcl/interp.py
@@ -19,7 +19,7 @@ class Interpreter():
             return None
 
     def eval(self, script):
-        tokens = script.split(' ')
+        tokens = self.tokenise(script)
         if len(tokens) > 0:
             self.call(tokens.pop(0), tokens)
 
@@ -28,8 +28,59 @@ class Interpreter():
             if len(args) == 2:
                 if args[1][0].isdigit():
                     self.variables[args[0]] = int(args[1])
+                elif args[1][0] == '"' and args[1][-1] == '"' and len(args[1]) > 2:
+                    self.variables[args[0]] = args[1][1:-1]
+                elif args[1][0] == "'" and args[1][-1] == "'" and len(args[1]) > 2:
+                    self.variables[args[0]] = args[1][1:-1]
+
                 else:
                     self.variables[args[0]] = args[1]
             else:
                 raise IncorrectNumberOfArgumentsError(f"set requires 2 arguments, {len(args)} given.")
+
+    def tokenise(self, script):
+        WAITING = 0
+        WAITING_FOR_SPACE = 1
+        MID_TOKEN = 2
+        DOUBLE_QUOTES = 3
+        SINGLE_QUOTES = 4
+        tokens = []
+        token = ""
+        state = WAITING
+        for i, char in enumerate(script):
+            # print(f"== {state} {char} -> ", end=' ')
+            if char == ' ':
+                if state == WAITING:
+                    pass
+                elif state == WAITING_FOR_SPACE:
+                    state = WAITING
+                elif state == MID_TOKEN:
+                    tokens.append(token)
+                    token = ""
+                    state = WAITING
+                else:
+                    token = token + char
+            elif state == WAITING_FOR_SPACE:
+                raise Exception(f"Expected space at character {i}")
+            elif char == '"' and state == WAITING:
+                state = DOUBLE_QUOTES
+            elif char == '"' and state == DOUBLE_QUOTES:
+                tokens.append(token)
+                token = ""
+                state = WAITING_FOR_SPACE
+            elif char == "'" and state == WAITING:
+                state = SINGLE_QUOTES
+            elif char == "'" and state == SINGLE_QUOTES:
+                tokens.append(token)
+                token = ""
+                state = WAITING_FOR_SPACE
+            else:
+                token = token + char
+                if state == WAITING:
+                    state = MID_TOKEN
+            # print(f"{state}   ({token})")
+        if state == MID_TOKEN and len(token) > 0:
+            tokens.append(token)
+
+        return tokens
 
---
+++ tests/test_set.py
@@ -0,0 +1,46 @@
+import unittest
+
+from pytcl.interp import Interpreter, IncorrectNumberOfArgumentsError
+
+
+class TestSetCommand(unittest.TestCase):
+    def test_simple_set_script(self):
+        interp = Interpreter()
+
+        interp.eval('set x 5')
+
+        self.assertEqual(interp.get('x'), 5)
+
+    def test_simple_set_script_incorrect_args(self):
+        interp = Interpreter()
+
+        with self.assertRaises(IncorrectNumberOfArgumentsError) as context:
+            interp.eval('set x')
+
+        self.assertTrue("set requires 2 arguments, 1 given." in str(context.exception))
+
+    def test_simple_set_script_incorrect_args_excess(self):
+        interp = Interpreter()
+
+        with self.assertRaises(IncorrectNumberOfArgumentsError) as context:
+            interp.eval('set x 5 4')
+
+        self.assertTrue("set requires 2 arguments, 3 given." in str(context.exception))
+
+    def test_simple_set_script(self):
+        interp = Interpreter()
+
+        interp.eval('set x "5"')
+
+        self.assertEqual(interp.get('x'), '5')
+
+    def test_string_set(self):
+        interp = Interpreter()
+
+        interp.eval('set x "This is a string"')
+
+        self.assertEqual(interp.get('x'), 'This is a string')
+
+
+if __name__ == '__main__':
+    unittest.main()
